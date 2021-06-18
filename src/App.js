437c1515ebef056a8dcf76d1708a665e72479d24
Samir Montalvan <montalvansamir5@gmail.com>
2021-06-17 22:40:12 -0500

    Show some gifs on page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
-
+const apiURL =
+  "https://api.giphy.com/v1/gifs/search?api_key=FSfiPYLhPksvm9GGBX9lwjRq5FyfUzJS&q=hola&limit=10&offset=0&rating=g&lang=en";
 
 function App() {
-  const { gifs, setGifs } = useState([]);
+  const [gifs, setGifs] = useState([]);
+
+  useEffect(function () {
+    fetch(apiURL)
+      .then((res) => res.json())
+      .then((response) => {
+        const { data } = response;
+        const gifs = data.map((image) => image.images.downsized_medium.url);
+        setGifs(gifs);
+      });
+  }, []);
+
   return (
     <div className="App">
       <section className="App-content">
-        <img src={gifs} alt="" />
+        {gifs.map((singleGif) => (
+          <img src={singleGif} />
+        ))}
       </section>
     </div>
-  ); 
+  );
 }
 /* FSfiPYLhPksvm9GGBX9lwjRq5FyfUzJS; */
 
